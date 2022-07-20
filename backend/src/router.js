f6e7838dea352c28489a39e0d2e6592d87dd91d6
Simon Duc <ducsimon@gmail.com>
2022-07-20 14:25:06 +0200

    create the client table routes

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const { ProjectController } = require("./controllers");
+const { ClientController } = require("./controllers");
 
 const router = express.Router();
 
@@ -9,5 +10,11 @@ router.get("/projects/:id", ProjectController.read);
 router.put("/projects/:id", ProjectController.edit);
 router.post("/projects", ProjectController.add);
 router.delete("/projects/:id", ProjectController.delete);
+
+router.get("/clients", ClientController.browse);
+router.get("/clients/:id", ClientController.read);
+router.put("/clients/:id", ClientController.edit);
+router.post("/clients", ClientController.add);
+router.delete("/clients/:id", ClientController.delete);
 
 module.exports = router;
