c116b19c14e7fdb39c5cdf2f6a9632a7a3cb5f1b
Simon Duc <ducsimon@gmail.com>
2022-07-20 15:14:39 +0200

    modify the find and findAll queries to get the client_name instead of the client_id in the project manager

--- backend/src/models/ProjectManager.js
+++ backend/src/models/ProjectManager.js
@@ -3,6 +3,19 @@ const AbstractManager = require("./AbstractManager");
 class ProjectManager extends AbstractManager {
   static table = "project";
 
+  find(id) {
+    return this.connection.query(
+      `select project.id, project.name, project.description, project.start_date, project.end_date, project.img_url, project.developer_team, project.linkedin_post_link, project.github_repo_link, client.name as client_name from  ${ProjectManager.table} inner join client on client.id = project.client_id where project.id = ?`,
+      [id]
+    );
+  }
+
+  findAll() {
+    return this.connection.query(
+      `select project.id, project.name, project.description, project.start_date, project.end_date, project.img_url, project.developer_team, project.linkedin_post_link, project.github_repo_link, client.name as client_name from project inner join client on client.id = project.client_id`
+    );
+  }
+
   insert(project) {
     return this.connection.query(
       `insert into ${ProjectManager.table} (name, description, start_date, end_date, img_url, developer_team, linkedin_post_link, github_repo_link, client_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
